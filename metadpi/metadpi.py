3df7d21a0ac5fad18f6d81641eb305a1cd5a6a2f
eved1018 <edelsteinevan@gmail.com>
2021-11-24 10:31:14 -0500

    c

--- metadpi/metadpi.py
+++ metadpi/metadpi.py
@@ -1,45 +1,60 @@
 import os
+
+from metadpi.scripts.containers.argscontainer import ArgsContainer
 from .scripts.userinterface import userinterface
 import pandas as pd
-pd.options.mode.chained_assignment = None  # default='warn'
 from .scripts.postprocess import postprocess
 from .scripts.crossvalidation import hyperparamtertuning_and_crossvalidation
 from .scripts.generate import generate
 from .scripts.predict import predict
 from .scripts.graphing import roc_viz, pr_viz, treeviz, pymol_viz
-from .scripts.preprocess import data_preprocesss, data_split_auto,data_split_from_file,cross_validation_set_generater
+from .scripts.preprocess import data_preprocesss, data_split_auto,\
+    data_split_from_file, cross_validation_set_generater
+pd.options.mode.chained_assignment = None  # default='warn'
 
 """
 TODO:
-1) change return type tuple to be Tuple[type1,type2...] ad inlude typesaftey on arguments!
+1) change return type tuple to be Tuple[type1,type2...] ad include type safety on arguments!
 2) for output dir: create nested folders for ease of access!
-3) make folder for all models to output into 
+3) make folder for all models to output into
 4) make cvout folder for all cv stuff
 """
 
-def main() -> None:
-    args_container = userinterface()         # get Command line arguments and defualts 
-    df = pd.read_csv(args_container.input_frames_file) # index_col = 0, wrap this in a try statement    #read input file containing residues, individual predictors and annotated columns
-    df, feature_cols, annotated_col, proteins  = data_preprocesss(df)   #preprocess data -> remove any null or missing data from the dataset and check that annoted is number  nulls 
-    predicted_col = feature_cols + ['logisticregresion', "linearregression",'randomforest'] + args_container.models_to_use    # make this automatic? if we do more models later change this!!! 
-    nn = args_container.nn
-    xg = args_container.xg
-    #Mode 1: predict 
-    
-    if args_container.mode == 'predict':
-        df = predict(df,feature_cols,args_container.input_folder_path,args_container.model_name,nn,xg)
-        results_df, roc_curve_data, pr_curve_data , bin_frame, stats_df= postprocess(df,predicted_col,args_container,annotated_col,args_container.autocutoff)
-        visualization(roc_curve_data,pr_curve_data ,None,df,feature_cols,annotated_col,predicted_col,df,bin_frame,args_container)
 
-    #Mode 2: generate learned model 
-    
-    elif  args_container.mode == 'generate':
+def main() -> None:
+    # get Command line arguments and defaults.
+    args_container: ArgsContainer = userinterface()
+    # index_col = 0, wrap this in a try statement | read input file containing residues, individual predictors and annotated columns.
+    df: pd.DataFrame = pd.read_csv(args_container.input_frames_file)
+    # preprocess data -> remove any null or missing data from the dataset and check that annoted is number  nulls.
+    df, feature_cols, annotated_col, proteins = data_preprocesss(df)
+    # make this automatic? if we do more models later change this!!!
+    predicted_col = feature_cols + \
+        ['logisticregresion', "linearregression",
+            'randomforest'] + args_container.models_to_use
+    nn: bool = args_container.nn
+    xg: bool = args_container.xg
+
+    # Mode 1: predict
+
+    if args_container.mode == 'predict':
+        df = predict(df, feature_cols, args_container.input_folder_path,
+                     args_container.model_name, nn, xg)
+        results_df, roc_curve_data, pr_curve_data, bin_frame, stats_df = postprocess(
+            df, predicted_col, args_container, annotated_col, args_container.autocutoff)
+        visualization(roc_curve_data, pr_curve_data, None, df, feature_cols,
+                      annotated_col, predicted_col, df, bin_frame, args_container)
+
+    # Mode 2: generate learned model
+
+    elif args_container.mode == 'generate':
         models, tree = generate(df, feature_cols, annotated_col, args_container.output_path_dir,
                                 args_container.model_name, args_container.rf_params, nn, xg)
-        if (tree is not None) and args_container.save_tree:    
-            treeviz(tree,df,feature_cols,annotated_col, args_container.model_name, args_container.output_path_dir)
+        if (tree is not None) and args_container.save_tree:
+            treeviz(tree, df, feature_cols, annotated_col,
+                    args_container.model_name, args_container.output_path_dir)
 
-    #Mode 3: test/train
+    # Mode 3: test/train
 
     elif args_container.mode == 'test':
 
@@ -47,36 +62,45 @@ def main() -> None:
             test_frame, train_frame = data_split_from_file(df, args_container)
         else:
             test_frame, train_frame = data_split_auto(df, proteins)
-        
-        print(f'lenght of test set: {len(test_frame)}', f"length of training set: {len(train_frame)}")
+
+        print(f'lenght of test set: {len(test_frame)}',
+              f"length of training set: {len(train_frame)}")
         models, tree = generate(train_frame, feature_cols, annotated_col, args_container.output_path_dir,
                                 args_container.model_name, args_container.rf_params, nn, xg)  # train
         # test
-        test_frame = predict(test_frame, feature_cols, args_container.input_folder_path, args_container.model_name, nn, xg, models)
-        results_df, roc_curve_data,pr_curve_data , bin_frame, fscore_mcc_by_protein, stats_df= postprocess(test_frame,predicted_col,args_container,annotated_col,args_container.autocutoff)
+        test_frame = predict(test_frame, feature_cols, args_container.input_folder_path,
+                             args_container.model_name, nn, xg, models)
+        results_df, roc_curve_data, pr_curve_data, bin_frame, fscore_mcc_by_protein, stats_df = postprocess(
+            test_frame, predicted_col, args_container, annotated_col, args_container.autocutoff)
         df_saver(results_df, "results", args_container.output_path_dir)
         df_saver(bin_frame, "bin_frame", args_container.output_path_dir)
-        df_saver(fscore_mcc_by_protein, "fscore_mcc_by_protein", args_container.output_path_dir)
-        df_saver(stats_df, "pairtest",args_container.output_path_dir )
-        visualization(roc_curve_data,pr_curve_data ,tree,df,feature_cols,annotated_col,predicted_col,test_frame ,bin_frame,args_container)
+        df_saver(fscore_mcc_by_protein, "fscore_mcc_by_protein",
+                 args_container.output_path_dir)
+        df_saver(stats_df, "pairtest", args_container.output_path_dir)
+        visualization(roc_curve_data, pr_curve_data, tree, df, feature_cols,
+                      annotated_col, predicted_col, test_frame, bin_frame, args_container)
         print(results_df)
 
+    # Mode 4: Cross-validation:
 
-    #Mode 4: Cross-validation: 
-    
-    elif  args_container.mode == 'cv':
-        test_frame, cvs,train_proteins = cross_validation_set_generater(args_container.cvs_path,df)
-        models = hyperparamtertuning_and_crossvalidation(df, train_proteins,feature_cols, annotated_col,args_container)
-        
+    elif args_container.mode == 'cv':
+        test_frame, cvs, train_proteins = cross_validation_set_generater(
+            args_container.cvs_path, df)
+        models = hyperparamtertuning_and_crossvalidation(
+            df, train_proteins, feature_cols, annotated_col, args_container)
 
-        model_param_writer(models, args_container.output_path_dir)    
-        test_frame = predict(test_frame,feature_cols,args_container.input_folder_path,args_container.model_name, nn,xg, models)
-        results_df, roc_curve_data,pr_curve_data , bin_frame, fscore_mcc_by_protein, stats_df= postprocess(test_frame,predicted_col,args_container,annotated_col,args_container.autocutoff)
+        model_param_writer(models, args_container.output_path_dir)
+        test_frame = predict(test_frame, feature_cols, args_container.input_folder_path,
+                             args_container.model_name, nn, xg, models)
+        results_df, roc_curve_data, pr_curve_data, bin_frame, fscore_mcc_by_protein, stats_df = postprocess(
+            test_frame, predicted_col, args_container, annotated_col, args_container.autocutoff)
         df_saver(results_df, "results", args_container.output_path_dir)
         df_saver(bin_frame, "bin_frame", args_container.output_path_dir)
-        df_saver(fscore_mcc_by_protein, "fscore_mcc_by_protein", args_container.output_path_dir)
-        df_saver(stats_df, "pairtest",args_container.output_path_dir )
-        visualization(roc_curve_data,pr_curve_data ,None,df,feature_cols,annotated_col,predicted_col,test_frame ,bin_frame,args_container)
+        df_saver(fscore_mcc_by_protein, "fscore_mcc_by_protein",
+                 args_container.output_path_dir)
+        df_saver(stats_df, "pairtest", args_container.output_path_dir)
+        visualization(roc_curve_data, pr_curve_data, None, df, feature_cols,
+                      annotated_col, predicted_col, test_frame, bin_frame, args_container)
         print(results_df)
 
     elif args_container.mode == 'viz':
@@ -86,38 +110,46 @@ def main() -> None:
         bin_frame = pd.read_csv(os.path.join(
             args_container.output_path_dir, "bin_frame.csv"), index_col=0)
         protein_to_viz = test_frame["protein"].unique()
-        pymol_viz(bin_frame, protein_to_viz, predicted_col,annotated_col, args_container.pymolscriptpath, args_container.output_path_dir)
+        pymol_viz(bin_frame, protein_to_viz, predicted_col, annotated_col,
+                  args_container.pymolscriptpath, args_container.output_path_dir)
 
     else:
         print("mode is set incorrectly")
     return
+
 
 def df_saver(df, name, output_path_dir):
     out = os.path.join(output_path_dir, f'{name}.csv')
     df.to_csv(out)
     return
 
+
 def model_param_writer(models, output_path_dir):
-    rf_model, linear_model, logit_model,NN_model ,xgb_model = models 
+    rf_model, linear_model, logit_model, NN_model, xgb_model = models
 
     ensable_param_dict = [f"{type(model).__name__}: {model.get_params()}\n"
-                          for model in filter(None,[rf_model, NN_model,xgb_model])]
+                          for model in filter(None, [rf_model, NN_model, xgb_model])]
 
     regr_param_dict = [f"{type(model).__name__}:    {model.coef_}\n"
                        for model in filter(None, [linear_model, logit_model])]
 
-    out = os.path.join(output_path_dir, f'best_parameters.txt')        
-    with open(out,'w+') as file:
+    out = os.path.join(output_path_dir, f'best_parameters.txt')
+    with open(out, 'w+') as file:
         file.writelines(regr_param_dict)
         file.writelines(ensable_param_dict)
     return
-    
-def visualization(roc_curve_data,pr_curve_data ,tree,df,feature_cols,annotated_col,predicted_col,test_frame, bin_frame,args_container):
-    roc_viz(roc_curve_data,args_container.output_path_dir, args_container.model_name)
-    pr_viz(pr_curve_data,args_container.output_path_dir,args_container.model_name, test_frame, annotated_col) 
-    if (tree is not None) and args_container.save_tree:    
-        treeviz(tree,df,feature_cols,annotated_col, args_container.model_name, args_container.output_path_dir)
+
+
+def visualization(roc_curve_data, pr_curve_data, tree, df, feature_cols, annotated_col, predicted_col, test_frame, bin_frame, args_container):
+    roc_viz(roc_curve_data, args_container.output_path_dir,
+            args_container.model_name)
+    pr_viz(pr_curve_data, args_container.output_path_dir,
+           args_container.model_name, test_frame, annotated_col)
+    if (tree is not None) and args_container.save_tree:
+        treeviz(tree, df, feature_cols, annotated_col,
+                args_container.model_name, args_container.output_path_dir)
     protein_to_viz = test_frame["protein"].unique()
     if args_container.usepymol:
-        pymol_viz(bin_frame, protein_to_viz, predicted_col,annotated_col, args_container.pymolscriptpath, args_container.output_path_dir)
+        pymol_viz(bin_frame, protein_to_viz, predicted_col, annotated_col,
+                  args_container.pymolscriptpath, args_container.output_path_dir)
     return
--- metadpi/scripts/containers/argscontainer.py
+++ metadpi/scripts/containers/argscontainer.py
@@ -2,35 +2,39 @@ import argparse
 import os
 import pathlib
 
+
 class ArgsContainer:
-    def __init__(self,args: argparse.Namespace, folder_path:pathlib.PosixPath) -> None:
-        self.args = args
-        self.mode = args.modeselection
-        self.folder_path  = folder_path
-        self.model_name = args.model_name
-        self.autocutoff = args.autocutoff
-        self.plotmode = args.plotselection
-        self.use_test_train_files = True
-        self.use_cutoff_from_file = True
-        self.save_tree = args.tree_visualization
-        self.usepymol = args.protein_visualization
-        self.xg = args.xgboost
-        self.nn = args.nuarelnet
-        self.models_to_use = []
+    def __init__(self, args: argparse.Namespace, folder_path: pathlib.PosixPath) -> None:
+        self.args: argparse.Namespace = args
+        self.mode: str = args.modeselection
+        self.folder_path: pathlib.PosixPath = folder_path
+        self.model_name: str = args.model_name
+        self.autocutoff: int = args.autocutoff
+        self.plotmode: str = args.plotselection
+        self.use_test_train_files: bool = True
+        self.use_cutoff_from_file: bool = True
+        self.save_tree: bool = args.tree_visualization
+        self.usepymol: bool = args.protein_visualization
+        self.xg: bool = args.xgboost
+        self.nn: bool = args.nuarelnet
+        self.models_to_use: list = []
         self.outputfolder = args.outputfolder
         self.inputfolder = args.inputfolder
-        self.cvs_path =  os.path.join(folder_path, self.inputfolder, args.cvfoldername)
-        self.output_path_dir= os.path.join(folder_path, self.outputfolder)
+        self.cvs_path = os.path.join(
+            folder_path, self.inputfolder, args.cvfoldername)
+        self.output_path_dir = os.path.join(folder_path, self.outputfolder)
         self.input_folder_path = os.path.join(folder_path, self.inputfolder)
-        self.input_frames_file = os.path.join(folder_path, self.inputfolder, args.inputfile)
-        self.test_proteins_file=  os.path.join(folder_path, self.inputfolder, args.testset)
-        self.train_proteins_file =  os.path.join(folder_path, self.inputfolder, args.trainset)
-        self.cutoff_frame = os.path.join(folder_path, self.inputfolder, args.cutoffs)
-        self.rf_params = [args.randomforest_parameter_trees, args.random_forest_parameter_depth, args.random_forest_parameter_ccp]
-        self.pymolscriptpath = os.path.join(folder_path, 'scripts', 'visualization', 'pymolviz.py')
+        self.input_frames_file = os.path.join(
+            folder_path, self.inputfolder, args.inputfile)
+        self.test_proteins_file = os.path.join(
+            folder_path, self.inputfolder, args.testset)
+        self.train_proteins_file = os.path.join(
+            folder_path, self.inputfolder, args.trainset)
+        self.cutoff_frame = os.path.join(
+            folder_path, self.inputfolder, args.cutoffs)
+        self.rf_params = [args.randomforest_parameter_trees,
+                          args.random_forest_parameter_depth, args.random_forest_parameter_ccp]
+        self.pymolscriptpath = os.path.join(
+            folder_path, 'scripts', 'visualization', 'pymolviz.py')
         return
-
-
- 
-
 
